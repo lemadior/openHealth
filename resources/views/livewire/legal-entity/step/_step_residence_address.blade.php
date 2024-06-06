bbe1650b20085262d95377289193c8e693adced2
Vladislav <vladprofmet@gmail.com>
2024-06-06 17:23:38 +0300

    add Addresses api adn fix double click

--- resources/views/livewire/legal-entity/step/_step_residence_address.blade.php
+++ resources/views/livewire/legal-entity/step/_step_residence_address.blade.php
@@ -3,7 +3,7 @@
     <h3>  {{  __('Крок :currentSteep з :totalSteps', ['currentSteep' => $currentStep,'totalSteps' => $totalSteps]) }}</h3>
 </x-slot>
 <div>
-<livewire:components.addresses-search  :addresses="$legal_entity_form->residence_address ?? []" :class="'grid grid-cols-2 gap-6'" />
+<livewire:components.addresses-search  :addresses="$legal_entity_form->addresses ?? []" :class="'grid grid-cols-2 gap-6'" />
 
 </div>
 
