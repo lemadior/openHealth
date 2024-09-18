9b7e4c251e76b7526d973a268c3f52978367ca06
Vladislav <vladprofmet@gmail.com>
2024-09-18 18:34:27 +0300

    contract form update

---
+++ app/Rules/CheckDateDifference.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Carbon\Carbon;
+
+class CheckDateDifference implements Rule
+{
+    protected string $startDate;
+
+    public function __construct($startDate)
+    {
+        $this->startDate = !empty($startDate) ? $startDate : Carbon::now()->format('d.m.Y');;
+    }
+
+    public function passes($attribute, $value)
+    {
+        $startDate = Carbon::createFromFormat('d.m.Y', $this->startDate);
+        $endDate = Carbon::createFromFormat('d.m.Y', $value);
+        return $endDate->diffInDays($startDate) <= 365;
+    }
+
+    public function message()
+    {
+        return __('Дата закінчення не може бути більше 1 року');
+    }
+}
